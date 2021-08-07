3e9ab40e0cc224c2ef19918b500f5a8515cbe86e
H3rmt <stemmer.enrico@gmail.com>
2021-08-07 04:24:16 +0200

    Calendar 5/? more Animations
    added move animation to dots

--- src/frame/Calendar table classes.kt
+++ src/frame/Calendar table classes.kt
@@ -5,12 +5,12 @@ import java.time.ZonedDateTime
 
 class Week {
 	lateinit var Monday: Day
-	lateinit var  Tuesday: Day
-	lateinit var  Wednesday: Day
-	lateinit var  Thursday: Day
-	lateinit var  Friday: Day
-	lateinit var  Saturday: Day
-	lateinit var  Sunday: Day
+	lateinit var Tuesday: Day
+	lateinit var Wednesday: Day
+	lateinit var Thursday: Day
+	lateinit var Friday: Day
+	lateinit var Saturday: Day
+	lateinit var Sunday: Day
 
 	var self: Week = this
 }
@@ -19,7 +19,17 @@ class Day(_time: ZonedDateTime) {
 
 	val time: ZonedDateTime = _time
 
+	var appointments: List<Appointment> = listOf()
+
 	override fun toString(): String {
 		return "${time.dayOfMonth}"//:${time.dayOfWeek}"
 	}
+}
+
+class Appointment(val _description: String, val _type: Types) {
+
+}
+
+enum class Types {
+	Work, School
 }
--- src/frame/Calendar.kt
+++ src/frame/Calendar.kt
@@ -1,7 +1,9 @@
 package frame
 
+import javafx.animation.Animation
 import javafx.animation.KeyFrame
 import javafx.animation.KeyValue
+import javafx.animation.PathTransition
 import javafx.animation.Timeline
 import javafx.collections.FXCollections
 import javafx.collections.ObservableList
@@ -14,17 +16,22 @@ import javafx.scene.control.TabPane
 import javafx.scene.control.TableCell
 import javafx.scene.control.TableColumn
 import javafx.scene.control.TableView
-import javafx.scene.layout.HBox
 import javafx.scene.layout.Pane
+import javafx.scene.layout.VBox
 import javafx.scene.paint.Color
+import javafx.scene.shape.CubicCurveTo
+import javafx.scene.shape.MoveTo
+import javafx.scene.shape.Path
 import javafx.util.Callback
 import javafx.util.Duration
 import logic.getLangString
 import tornadofx.action
+import tornadofx.add
 import tornadofx.addClass
 import tornadofx.box
 import tornadofx.button
 import tornadofx.circle
+import tornadofx.clear
 import tornadofx.column
 import tornadofx.getChildList
 import tornadofx.hbox
@@ -87,112 +94,191 @@ fun GetMonth(month: Int): MutableList<Week> {
 			Time = Time.plusDays(1)
 		} while(Time.dayOfWeek.value != 1)
 
+		newWeek.Friday.appointments = listOf(
+			Appointment("Arb", Types.Work),
+			Appointment("School", Types.School),
+			Appointment("School", Types.School),
+			Appointment("Arb 3", Types.Work)
+		)
+		newWeek.Saturday.appointments = listOf(Appointment("Arb 2", Types.Work), Appointment("Arb 3", Types.Work))
+		newWeek.Thursday.appointments = listOf(Appointment("Mathe", Types.School))
+
 		weeks.add(weeks.size, newWeek)
 	} while(Time.month.value == month && Time.dayOfMonth > 1)
 
 	return weeks
 }
 
-fun createGraphics(celldata: Any?, graphic: Node): Pane? {
+fun createGraphics(celldata: Any?, graphic: TableCell<Week, Any>): Array<Any?>? {
 	if(celldata == null)
 		return null
+
+	var generateddata: Array<Any>? = null
+
 	val graphicContainer = graphic.vbox {
 		addClass(Styles.CalendarView.tablecellpane)
 		style(append = true) {
 			alignment = Pos.TOP_CENTER
 		}
 
-		label {
-			addClass(Styles.CalendarView.celllabel)
-			if(celldata is Day) {
+		if(celldata is Day) {
+			label {
+				addClass(Styles.CalendarView.celllabel)
 				text = celldata.toString()
 			}
-		}
-
-		if(celldata is Day) {
-			hbox {
-				style(append = true) {
-					padding = box(4.px)
-					spacing = 5.px
-					alignment = Pos.CENTER
-				}
-				circle(radius = 5.0) {
-					style(append = true) {
-						fill = Color.RED
-					}
-				}
-				circle(radius = 5.0) {
-					style(append = true) {
-						baseColor = Color.RED
-					}
-				}
-			}
+			generateddata = generateDayGraphic(celldata, this)
+			add(generateddata !![0] as Node)
 		}
 	}
-	return graphicContainer
+	return arrayOf(graphicContainer, generateddata?.get(1), generateddata?.get(2))
 }
 
+fun generateDayGraphic(day: Day, vBox: VBox): Array<Any> {
+	val pane = vBox.pane {
+		style {
+			//backgroundColor += Color.RED
+			prefHeight = 10.px
+		}
+	}
+
+	var first = true
+
+	val openTransitions = mutableListOf<PathTransition>()
+	val closeTransitions = mutableListOf<PathTransition>()
+
+	pane.widthProperty().addListener {_ ->
+		if(! first || day.appointments.isEmpty())
+			return@addListener
+
+		first = false
+		pane.clear()
+
+		val xcords = mutableListOf<Double>()
+
+		val width = pane.width.toInt().toDouble()
+		val spacing = 5
+		val circlewidth = 6
+
+		val vtopmargin = 4.0
+		val hleftmargin = 8.0
+
+		if(day.appointments.size % 2 == 0) {
+			for(index in 0 until day.appointments.size / 2) {
+				xcords.add((width / 2) + ((spacing / 2) + (index * (circlewidth + spacing)) + circlewidth / 2))
+				xcords.add((width / 2) + ((spacing / 2) + (index * (circlewidth + spacing)) + circlewidth / 2) * - 1)
+			}
+		} else {
+			xcords.add(width / 2)
+			for(index in 0 until (day.appointments.size - 1) / 2) {
+				xcords.add((width / 2) + ((circlewidth / 2) + spacing + (index * (circlewidth + spacing)) + circlewidth / 2))
+				xcords.add((width / 2) + ((circlewidth / 2) + spacing + (index * (circlewidth + spacing)) + circlewidth / 2) * - 1)
+			}
+		}
+
+		// else from middle to center
+		xcords.sortDescending()
+
+		for((index, appointment) in day.appointments.withIndex()) {
+			pane.add(pane.circle(radius = circlewidth / 2) {
+				fill = colormap[appointment._type]
+				centerY = vtopmargin
+				centerX = xcords[index]
+			})
+		}
+
+		val ycords = mutableListOf<Double>()
+		for(index in 0 until day.appointments.size) {
+			ycords.add(8.0 + index * (spacing + circlewidth))
+		}
+
+		for(index in 0 until day.appointments.size) {
+			val openpath = Path()
+			openpath.elements.add(MoveTo(xcords[index], vtopmargin))
+			openpath.elements.add(
+				CubicCurveTo(
+					xcords[index], vtopmargin, hleftmargin * 1.8, vtopmargin * 1.8, hleftmargin, ycords[index]
+				)
+			)
+			openTransitions.add(PathTransition(Duration(300.0), openpath, pane.getChildList()?.get(index)))
+
+			val closepath = Path()
+			closepath.elements.add(MoveTo(hleftmargin, ycords[index]))
+			closepath.elements.add(
+				CubicCurveTo(
+					hleftmargin, ycords[index], xcords[index], ycords[index], xcords[index], vtopmargin
+				)
+			)
+			closeTransitions.add(PathTransition(Duration(200.0), closepath, pane.getChildList()?.get(index)))
+		}
+	}
+
+	return arrayOf(pane, openTransitions, closeTransitions)
+}
+
+val colormap: Map<Types, Color> = mapOf(
+	Types.Work to Color.CYAN,
+	Types.School to Color.GOLD,
+)
+
+// Animations
+
+val openvalues = arrayOf( // duration, cell height, taskpane height
+	arrayOf(0.0, 46, 10),
+	arrayOf(40.0, 56, 30),
+	arrayOf(160.0, 80, 40),
+	arrayOf(200.0, 90, 55)
+)
+
+val closevalues = arrayOf( // duration, cell height, taskpane height
+	arrayOf(200.0, 46, 10),
+	arrayOf(160.0, 56, 30),
+	arrayOf(40.0, 80, 40),
+	arrayOf(0.0, 90, 55)
+)
 
 fun <T> cellfactory(): Callback<TableColumn<Week, T>, TableCell<Week, T>> {
 	return Callback<TableColumn<Week, T>, TableCell<Week, T>> {
 		val cell: TableCell<Week, T> = TableCell()
 
-		val open = Timeline()
-		val close = Timeline()
+		val opentimeline = Timeline()
+		var open: MutableList<Animation> = mutableListOf()
+		val closetimeline = Timeline()
+		var close: MutableList<Animation> = mutableListOf()
 
 		cell.itemProperty().addListener {_, _, day2 ->
-			createGraphics(day2, cell)?.also {it ->
-				cell.graphic = it
+			createGraphics(day2, cell as TableCell<Week, Any>)?.also {graphicdata ->
+				cell.graphic = graphicdata[0] as Node?
 				cell.addClass(Styles.CalendarView.tablecell)
 
-				open.keyFrames.clear()
-				close.keyFrames.clear()
+				open.clear()
+				close.clear()
 
-				val circles = it.getChildList()?.filterIsInstance<HBox>()?.let {
-					if (it.isNotEmpty())
-						return@let it[0].getChildList()
-					else
-						return@let listOf()
-				} ?: listOf()
+				val pane = (graphicdata[0] as Node).getChildList()?.filterIsInstance<Pane>()?.firstOrNull()
 
-				val openvalues = arrayOf( // duration,height,opacity
-					arrayOf(0.0, 46, 1.0),
-					arrayOf(20.0, 56, 0.5),
-					arrayOf(80.0, 80, 0.2),
-					arrayOf(100.0, 90, 0.0)
-				)
+				pane?.let {
+					for(value in openvalues) {
+						val keyvalues = mutableListOf<KeyValue>()
+						keyvalues.add(KeyValue(cell.minHeightProperty(), value[1] as Number))
+						keyvalues.add(KeyValue(it.minHeightProperty(), value[2] as Number))
 
-				val closevalues = arrayOf( // duration,height,opacity
-					arrayOf(100.0, 46, 1.0),
-					arrayOf(80.0, 56, 0.5),
-					arrayOf(20.0, 80, 0.2),
-					arrayOf(0.0, 90, 0.0)
-				)
-
-				for(value in openvalues) {
-					val keyvalues = mutableListOf<KeyValue>()
-					keyvalues.add(KeyValue(cell.minHeightProperty(), value[1] as Number))
-					for(circle in circles) {
-						keyvalues.add(KeyValue(circle.opacityProperty(), value[2] as Double))
-					}
-					open.keyFrames.add(
-						KeyFrame(
-							Duration(value[0] as Double), *keyvalues.toTypedArray()
+						opentimeline.keyFrames.add(
+							KeyFrame(
+								Duration(value[0] as Double), *keyvalues.toTypedArray()
+							)
 						)
-					)
-				}
-
-				for(value in closevalues) {
-					val keyvalues = mutableListOf<KeyValue>()
-					keyvalues.add(KeyValue(cell.minHeightProperty(), value[1] as Number))
-					for(circle in circles) {
-						keyvalues.add(KeyValue(circle.opacityProperty(), value[2] as Double))
 					}
-					close.keyFrames.add(
-						KeyFrame(
-							Duration(value[0] as Double), *keyvalues.toTypedArray()
+
+					for(value in closevalues) {
+						val keyvalues = mutableListOf<KeyValue>()
+						keyvalues.add(KeyValue(cell.minHeightProperty(), value[1] as Number))
+						keyvalues.add(KeyValue(it.minHeightProperty(), value[2] as Number))
+						closetimeline.keyFrames.add(
+							KeyFrame(Duration(value[0] as Double), *keyvalues.toTypedArray())
 						)
-					)
+					}
+
+					open = graphicdata[1] as MutableList<Animation>
+					close = graphicdata[2] as MutableList<Animation>
 				}
 			}
 		}
@@ -200,12 +286,17 @@ fun <T> cellfactory(): Callback<TableColumn<Week, T>, TableCell<Week, T>> {
 		cell.onMouseEntered = EventHandler {
 			(it.target as Node).getChildList()?.get(0)?.addClass(Styles.CalendarView.hoveredtablecellpane)
 			(it.target as Node).addClass(Styles.CalendarView.hoveredtablecell)
-			open.play()
+			open.forEach {animation -> animation.play()}
+			opentimeline.play()
+
 		}
 		cell.onMouseExited = EventHandler {
 			(it.target as Node).getChildList()?.get(0)?.removeClass(Styles.CalendarView.hoveredtablecellpane)
 			(it.target as Node).removeClass(Styles.CalendarView.hoveredtablecell)
-			close.play()
+			open.forEach {animation -> animation.stop()}
+			opentimeline.stop()
+			close.forEach {animation -> animation.play()}
+			closetimeline.play()
 		}
 		return@Callback cell
 	}
