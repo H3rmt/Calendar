bd083a639968fb117bb59b54e3fb355e9f6cb019
H3rmt <stemmer.enrico@gmail.com>
2021-09-06 00:25:47 +0200

    Notes Tab name
    added description for week and day in note tab

--- src/frame/Note.kt
+++ src/frame/Note.kt
@@ -1,16 +1,25 @@
 package frame
 
 import calendar.Celldisplay
+import calendar.Day
+import calendar.Week
 import javafx.scene.control.*
 import javafx.scene.paint.*
 import logic.LogType
+import logic.getLangString
 import logic.log
 import tornadofx.*
+import java.time.temporal.ChronoField
 
 
 fun createnotetab(pane: TabPane, cell: Celldisplay): Tab {
 	log("creating week tab", LogType.IMPORTANT)
-	return pane.tab("note ") { // "Notes for ${data.time.dayOfMonth}/${data.time.month}"
+	return pane.tab("") {
+		if(cell is Day)
+			text = "Notes for ${cell.time.dayOfMonth}. ${getLangString(cell.time.month.name)}"
+		else if(cell is Week)
+			text = "Notes for ${cell.time.get(ChronoField.ALIGNED_WEEK_OF_MONTH)}. Week in ${getLangString(cell.time.month.name)}"
+		
 		isClosable = true
 		stackpane {
 			style(append = true) {
